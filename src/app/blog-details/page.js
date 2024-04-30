63422a458b296de313738d9f03453c07f9e5cac7
divyeshfortune <divyesh.gothi1@gmail.com>
2024-04-30 12:47:48 +0530

    blog responsive done

--- src/app/blog-details/page.js
+++ src/app/blog-details/page.js
@@ -9,11 +9,11 @@ const Blogdetails = () => {
   return (
     <>
       <Header />
-      <section className="pt-[6.75rem]">
+      <section className="md:pt-[6.75rem] pt-8">
         <div className="container">
           <div className="blog_main">
-            <div className="grid grid-cols-12 pb-[3.5625rem]">
-              <div className="row-start-1 col-start-1 row-end-2 col-end-9">
+            <div className="grid md:grid-cols-12 grid-cols-1 pb-[3.5625rem]">
+              <div className="col-span-8">
                 <div className="pb-[1.375rem]">
                   <span className="blog_hashtage">Trends</span>
                   <span className="text-[#95AEC6] text-base font-normal border-r border-solid border-[#95AEC6] px-3">
@@ -23,9 +23,9 @@ const Blogdetails = () => {
                     13 Comments
                   </span>
                 </div>
-                <h1>Augue OLMO, luctus neque purus and ipsum neque libero</h1>
+                <h1 className="md:pb-0 pb-7">Augue OLMO, luctus neque purus and ipsum neque libero</h1>
               </div>
-              <div className="flex items-center gap-6 row-start-1 col-start-10 row-end-2 col-end-13">
+              <div className="flex items-center md:justify-center justify-start gap-6 col-span-4">
                 <Image
                   src="/images/4bc07465155a0f49705c0972a9e05e42.png"
                   alt="Author"
@@ -53,15 +53,15 @@ const Blogdetails = () => {
                 layout="responsive"
                 quality={100}
                 sizes="100vh"
-                className="rounded-[1.875rem] w-[73.125rem] !h-[47.8125rem]"
+                className="rounded-[1.875rem] w-[73.125rem] md:!h-[47.8125rem] !h-[16.8125rem]"
               />
             </div>
-            <div className="grid grid-cols-12 gap-[1.875rem] mt-12">
-              <div className="col-span-8">
+            <div className="grid sm:grid-cols-12 grid-cols-1 gap-[1.875rem] mt-12">
+              <div className="md:col-span-8 col-span-10">
                 <h2 className="pb-8">Main Heading & Points</h2>
                 <p className="text-18 text-secondary font-normal pb-12">
                   Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
+                  typesetting industry. Lorem Ipsum has been the industrys
                   standard dummy text ever since the 1500s, when an unknown when
                   an unknown printer took a galley of type and scrambled it to
                   make a type specimen book. It has survived not only five
@@ -88,20 +88,20 @@ const Blogdetails = () => {
                   sizes="100vh"
                 />
                 <p className="text-secondary py-7 text-18 font-normal]">Unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the</p>
-                <p className="text-28 font-normal text-midnight_text py-7 border-l-[0.1875rem] border-solid border-primary pl-8">We are a dedicated team of passionate product managers,developers, UX/UI designers, QA engineers experts helping businesses from new startups</p>
-                <p className="text-18 font-normal text-secondary py-7">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable making this the first true generator on the Internet. It uses a dictionary </p>
+                <p className="md:text-28 text-xl font-normal text-midnight_text py-7 border-l-[0.1875rem] border-solid border-primary pl-8">We are a dedicated team of passionate product managers,developers, UX/UI designers, QA engineers experts helping businesses from new startups</p>
+                <p className="text-18 font-normal text-secondary py-7">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable making this the first true generator on the Internet. It uses a dictionary </p>
                 <div className="flex gap-5 py-7 border-t border-b border-solid border-[#E1F1F6]">
                     <div>
                         <p className="text-22 font-medium text-midnight_text">Tags</p>
                     </div>
-                    <div className="flex items-center gap-[0.625rem]">
+                    <div className="flex sm:flex-row flex-col md:items-center items-start gap-[0.625rem]">
                         <span className="blog_hashtage">Trends</span>
                         <span className="blog_hashtage">Design</span>
                         <span className="blog_hashtage">Research</span>
                     </div>
                 </div>
               </div>
-              <div className="col-span-4 bg-white shadow-[0px_20px_80px_0px_#68758D26] px-10 rounded-lg h-[42.5%]">
+              <div className="md:col-span-4 col-span-10 bg-white shadow-[0px_20px_80px_0px_#68758D26] px-10 rounded-lg md:h-[42.5%] h-full md:pb-0 pb-4">
                 <span className="text-24 font-medium text-midnight_text inline-block py-6">Share</span>
                 <div className="flex flex-col gap-[0.875rem] pb-12 border-b border-solid border-[#F4F5F6]">
                     <Link href="#" className="bg-[#526FA3] py-[0.90rem] rounded-lg text-xl font-medium text-white flex items-center gap-[1.5rem] px-6 hover:bg-[#3e537a] hover:scale-[1.1] transition-all duration-500">
